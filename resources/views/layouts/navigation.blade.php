d04871b3c408d4b68450e735d8317872262a59f1
Vasu Naik <vasunaik1999@gmail.com>
2021-07-01 22:30:48 +0530

    Bug fixed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,6 +70,9 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('postcard.create')" :active="request()->routeIs('postcard.create')">
+                {{ __('Postcards') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
